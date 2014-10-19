0d60fea477768a3d8e09e5c4904296e26c724d11
Seneda <senedanassirali@gmail.com>
2014-10-19 22:29:58 +0100

    Add a script to cycle through shaking each leg

--- Tests/Walking/Gaits/simpleGait.py
+++ Tests/Walking/Gaits/simpleGait.py
@@ -1,11 +1,15 @@
 from servo.moveJoints import moveJoint
 
-moveJoint(4,300,600,3)
-moveJoint(5,300,600,3)
-moveJoint(8,300,600,3)
-moveJoint(9,300,600,3)
-
-moveJoint(4,600,300,3)
-moveJoint(5,600,300,3)
-moveJoint(8,600,300,3)
-moveJoint(9,600,300,3)
+while True:
+	try:	
+		moveJoint(4,300,600,1)
+		moveJoint(5,300,600,1)
+		moveJoint(8,300,600,1)
+		moveJoint(9,300,600,1)
+	
+		moveJoint(4,600,300,1)
+		moveJoint(5,600,300,1)
+		moveJoint(8,600,300,1)
+		moveJoint(9,600,300,1)
+	except KeyboardInterrupt:
+    		break
