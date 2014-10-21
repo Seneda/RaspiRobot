40b342df646bc2a5b64a9772579ca0a9b03561c3
Seneda <senedanassirali@gmail.com>
2014-10-21 22:03:20 +0100

    centre legs

--- Tests/Walking/Gaits/simpleGait.py
+++ Tests/Walking/Gaits/simpleGait.py
@@ -1,15 +1,22 @@
 from servo.moveJoints import moveJoint
 
-while True:
-	try:	
-		moveJoint(4,300,600,1)
-		moveJoint(5,300,600,1)
-		moveJoint(8,300,600,1)
-		moveJoint(9,300,600,1)
-	
-		moveJoint(4,600,300,1)
-		moveJoint(5,600,300,1)
-		moveJoint(8,600,300,1)
-		moveJoint(9,600,300,1)
-	except KeyboardInterrupt:
-    		break
+def centre():
+    moveJoint(4, 400, 400, 1)
+    moveJoint(5, 400, 400, 1)
+    moveJoint(8, 400, 400, 1)
+    moveJoint(9, 400, 400, 1)
+
+centre()
+#while True:
+#	try:	#
+#		moveJoint(4,300,600,1)
+#		moveJoint(5,300,600,1)
+#		moveJoint(8,300,600,1)
+#		moveJoint(9,300,600,1)
+#
+#		moveJoint(4,600,300,1)
+#		moveJoint(5,600,300,1)
+#		moveJoint(8,600,300,1)
+#		moveJoint(9,600,300,1)
+#	except KeyboardInterrupt:
+ #   		break
