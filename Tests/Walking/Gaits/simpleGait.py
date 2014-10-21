8264b9d82b29c36e19348ff083279ff172b93979
Seneda <senedanassirali@gmail.com>
2014-10-21 22:16:43 +0100

    centre legs

--- Tests/Walking/Gaits/simpleGait.py
+++ Tests/Walking/Gaits/simpleGait.py
@@ -3,8 +3,8 @@ from servo.moveJoints import moveJoint
 def centre():
     moveJoint(4, 400, 400, 1)
     moveJoint(5, 400, 400, 1)
-    moveJoint(8, 400, 400, 1)
-    moveJoint(9, 400, 400, 1)
+    moveJoint(8, 450, 450, 1)
+    moveJoint(9, 300, 300, 1)
     moveJoint(6, 400, 400, 1)
     moveJoint(7, 400, 400, 1)
     moveJoint(10, 400, 400, 1)
