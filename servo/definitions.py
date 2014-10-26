3d73e6540a2a087e4cb42589cab872946caa6a7c
Seneda <senedanassirali@gmail.com>
2014-10-26 21:27:48 +0000

    step forwards

--- servo/definitions.py
+++ servo/definitions.py
@@ -50,10 +50,11 @@ class Robot(object):
         for l in self.legs.values():
             l.hip.move(0.5,0.5,0.01)
             l.knee.move(0.3,0.3,0.01)
+        time.sleep(5)
         for l in [self.legs['FR'],self.legs['BL']]:
             l.knee.move(0.3,0.8,0.01)
             l.hip.move(0.5,0.8,0.01)
-        time.sleep(1)
+        time.sleep(5)
         for l in [self.legs['FR'],self.legs['BL']]:
             l.knee.move(0.8,0.3,0.01)
             l.hip.move(0.8,0.5,0.01)
