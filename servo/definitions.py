d0a7b4bdbdf829b83652762ebb5fdb2af7ecc669
Seneda <senedanassirali@gmail.com>
2014-10-26 20:40:16 +0000

    stand function

--- servo/definitions.py
+++ servo/definitions.py
@@ -41,9 +41,9 @@ class Robot(object):
     def __init__(self, legs):
         self.legs = legs
 
-    def stand(self):
+    def stand(self,start=0.8,stop=0.2):
         for l in self.legs.values():
-            l.knee.move(0.8,0.2)
+            l.knee.move(start,stop)
 
 
 robot = Robot(legs)
