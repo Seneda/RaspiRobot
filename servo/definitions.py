b9799359787e3f0484d9ade3a4fa09a8df6b5b4c
Seneda <senedanassirali@gmail.com>
2014-10-26 20:13:41 +0000

    test routines

--- servo/definitions.py
+++ servo/definitions.py
@@ -16,11 +16,11 @@ class joint(object):
 
 class hipJoint(joint):
     def __init__(self, pin, frontmax, backmax):
-        super(hipJoint,self).__init__(pin, frontmax, backmax)
+        super(hipJoint).__init__(pin, frontmax, backmax)
 
 class kneeJoint(joint):
     def __init__(self, pin, outmax, undermax):
-        super(hipJoint,self).__init__(pin, outmax, undermax)
+        super(hipJoint).__init__(pin, outmax, undermax)
 
 
 class leg(object):
@@ -29,11 +29,11 @@ class leg(object):
         self.knee = knee
 
 
-#Legs = {'FR':(leg(hipJoint(9,150,450),kneeJoint(11,225,500))),
-#        'FL':leg(hipJoint(8,600,300),kneeJoint(10,250,550)),
-#        'BR':leg(hipJoint(5,200,550),kneeJoint(7,250,550)),
-#        'BL':leg(hipJoint(4,500,200),kneeJoint(6,200,500)),
-#        }
+Legs = {'FR':(leg(hipJoint(9,150,450),kneeJoint(11,225,500))),
+       'FL':leg(hipJoint(8,600,300),kneeJoint(10,250,550)),
+       'BR':leg(hipJoint(5,200,550),kneeJoint(7,250,550)),
+       'BL':leg(hipJoint(4,500,200),kneeJoint(6,200,500)),
+       }
 
 #Legs = [(leg(hipJoint(9,150,450),kneeJoint(11,225,500))),
 #        leg(hipJoint(8,600,300),kneeJoint(10,250,550)),
