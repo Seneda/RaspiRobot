7688c9faa24087959fa0eed5e6a44abf7d3eb010
Seneda <senedanassirali@gmail.com>
2014-10-26 20:01:34 +0000

    test routines

--- servo/moveJoints.py
+++ servo/moveJoints.py
@@ -4,7 +4,7 @@ from Adafruit_Raspberry_Pi_Python_Code.Adafruit_PWM_Servo_Driver.Adafruit_PWM_Se
 
 #Legs Definition, (a,b,c) a=topservo pin b= bottom servo pin c = orientation
 
-leg = {"FR":(,),"FL":(,),"BR":(,),"BL":(,)}
+#leg = {"FR":(,),"FL":(,),"BR":(,),"BL":(,)}
 servoMin = 150
 servoMax = 600
 pwm = PWM(0x40, debug=True)
