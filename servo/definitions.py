c7ad3fd4204e0f42ab77167f9d527d724faa0812
Seneda <senedanassirali@gmail.com>
2014-10-26 20:21:39 +0000

    test routines

--- servo/definitions.py
+++ servo/definitions.py
@@ -1,7 +1,7 @@
 __author__ = 'seneda'
 from servo.moveJoints import moveJoint, stop
 
-class joint(object):
+class Joint(object):
     def __init__(self, pin, max, min):
         self.pin = pin
         self.max = max
@@ -14,26 +14,25 @@ class joint(object):
     def stop(self):
         stop(self.pin)
 
-class hipJoint(joint):
+class HipJoint(Joint):
     def __init__(self, pin, frontmax, backmax):
-        super(hipJoint).__init__(pin, frontmax, backmax)
+        super(HipJoint).__init__(pin, frontmax, backmax)
 
-class kneeJoint(joint):
+class KneeJoint(Joint):
     def __init__(self, pin, outmax, undermax):
-        super(hipJoint).__init__(pin, outmax, undermax)
+        super(HipJoint).__init__(pin, outmax, undermax)
 
 
-class leg(object):
+class Leg(object):
     def __init__(self,hip,knee):
         self.hip = hip
         self.knee = knee
 
 
-Legs = {'FR':(leg(hipJoint(9,150,450),kneeJoint(11,225,500))),
-       'FL':leg(hipJoint(8,600,300),kneeJoint(10,250,550)),
-       'BR':leg(hipJoint(5,200,550),kneeJoint(7,250,550)),
-       'BL':leg(hipJoint(4,500,200),kneeJoint(6,200,500)),
-
+Legs = {'FR':(Leg(HipJoint(9,150,450),KneeJoint(11,225,500))),
+       'FL':Leg(HipJoint(8,600,300),KneeJoint(10,250,550)),
+       'BR':Leg(HipJoint(5,200,550),KneeJoint(7,250,550)),
+       'BL':Leg(HipJoint(4,500,200),KneeJoint(6,200,500)),
        }
 
 #Legs = [(leg(hipJoint(9,150,450),kneeJoint(11,225,500))),
